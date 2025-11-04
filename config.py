832d308899e695aebe788c5528efb418638f91e3
Kaviiyyappa <93697664+Kaviiyyappa@users.noreply.github.com>
2025-11-04 09:03:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,14 +5,13 @@
 from os import environ 
 
 class Config:
-    API_ID = int(environ.get("API_ID", ""))
-    API_HASH = environ.get("API_HASH", "")
+    API_ID = int(environ.get("API_ID", "20491646"))
+    API_HASH = environ.get("API_HASH", "ac7a9044852c52eb3d61c07a7e1670bd")
     BOT_TOKEN = environ.get("BOT_TOKEN", "") 
-    BOT_SESSION = environ.get("BOT_SESSION", "vjbot") 
-    DATABASE_URI = environ.get("DATABASE_URI", "")
-    DATABASE_NAME = environ.get("DATABASE_NAME", "vj-forward-bot")
-    BOT_OWNER = int(environ.get("BOT_OWNER", ""))
-
+    BOT_SESSION = environ.get("BOT_SESSION", "IKBRYTbot") 
+    DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://workwithsaikat:saikat9735@cluster0.0e5vp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    DATABASE_NAME = environ.get("DATABASE_NAME", "IKBRYT-forward-bot")
+    BOT_OWNER = int(environ.get("BOT_OWNER", "8375678896"))
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
@@ -27,3 +26,4 @@ class temp(object):
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
+
